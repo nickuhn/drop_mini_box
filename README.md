081f4ef14993a48a26254877ca57223cd933ee13
Nick Kuhn <nickuhn@gmail.comn>
2015-07-25 16:23:11 -0700

    Refactored files routes
    files routes now follow same usage as user routes.
    Async is handled by event emitters.
    Update Readme with route listings.
    Updated whitespace and formatting.

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 #Express Server with S3 Persistence
+
+This project is an Express server, that takes and responds with JSON.
+
+It stores the data taken in on the hard drive and a backup on AWS S3. It also stores a user on Mongolabs that tracks the files url and owner.
+
+##Routes
+
+### api/users
+
+GET - Returns a list of the users stored in DB with files listed by their ids.
+
+POST {name: "username"} - Creates a new user in DB. Creates new folder with user name on hard drive. Creates a new bucket on S3.
+
+### api/users/:user
+
+GET - Returns the specified user from the DB with the file field populated.
+
+PUT {name: "username"} - Renames the user in DB. Renames the folder on hard drive. Renames bucket and associated files on S3.
+
+DELETE - Removes specified user from the DB. Deletes their folder from hard drive. Deletes bucket and all associated files on S3.
+
+### api/users/:user/files
+
+GET - Retrieves a list of files from DB for specified user.
+
+POST {name: "filename", content: "contents of file"} - Stores a new file on S3. Stores a new file on the hard drive. Adds a file to db containing a link to the file on S3.
+
+DELETE - Deletes all files for the specified user from S3, hard drive, and DB.
+
+### api/users/:user/files/:file
+
+GET - Returns the specified file from DB with link populated to download from S3.
+
+PUT {name: "newName", content: "newContent" } - Updates content or name of file stored in DB and on hard drive.
+
+DELETE - Removes specified file from S3, hard drive, and DB
+
--- routes/files-routes.js
+++ routes/files-routes.js
@@ -1,13 +1,25 @@
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var File = require(__dirname + '/../models/File');
-var User = require(__dirname + '/../models/User');
-var AWS = require('aws-sdk');
-var s3 = new AWS.S3();
+var fs           = require('fs');
+var AWS          = require('aws-sdk');
+var File         = require(__dirname + '/../models/File');
+var User         = require(__dirname + '/../models/User');
+var bodyParser   = require('body-parser');
+var EventEmitter = require('events').EventEmitter;
+var s3           = new AWS.S3();
+var s3done       = false;
+var hdDone       = false;
+var mongodbDone  = false;
 var parentBucket = 'nickuhnbucket';
 
 module.exports = function(router) {
   router.use(bodyParser.json());
+
+  function sendRes(res, message) {
+    if (message === 'error') {
+      res.status(500).json({msg: 'server error'});
+    } else if (s3done && mongodbDone && hdDone) {
+      res.json({msg: message});
+    }
+  }
 
   router.route('/users/:user/files')
     //Gets a list of files for a specific user
@@ -18,87 +30,141 @@ module.exports = function(router) {
             .populate('files')
             .exec(function(err, user) {
               if (err) {
-                res.send(err);
+                sendRes(res, 'error');
               }
               res.json(user.files);
             });
     })
     //Post a new file for a user
     //writes the file to the hard drive in folder with user name
-    //then links the file to the user in the mongodb
     //stores the file on s3 and stores the url for the file in the mongodb file
+    //then links the file to the user in the mongodb
     .post(function(req, res) {
-      fs.writeFile(__dirname + '/../files/' + req.params.user +'/' + req.body.name, req.body.content, function(err) {
+      var ee      = new EventEmitter();
+      var message = 'succesfully created file: ' + req.body.name;
+      var dirFile = req.params.user +'/' + req.body.name;
+      s3done      = false;
+      mongodbDone = false;
+      hdDone      = false;
+
+      s3.putObject({ Bucket: parentBucket, Key: dirFile, Body: req.body.content }, function(err, data) {
         if (err) {
-          res.send(err);
+          sendRes(res, 'error');
         } else {
-          var file = new File();
-          file.name = req.body.name;
-          User.findOne({name: req.params.user}, function(err, found) {
-            if (err) {
-              res.send(err);
-            }
-            found.files.push(file._id);
-            file.user = found._id;
-            found.save(function(err, file) {
-              if (err) {
-                res.send(err);
-              }
-            });
-            s3.putObject({ Bucket: parentBucket, Key: req.params.user +'/' + req.body.name, Body: req.body.content }, function(err, data) {
-              if (err) {
-                res.send(err);
-              } else {
-                file.url = s3.getSignedUrl('getObject', {Bucket: parentBucket, Key: req.params.user +'/' + req.body.name});
-                file.save(function(err, file) {
-                  if (err) {
-                    res.send(err);
-                  } else {
-                    res.send(file);
-                  }
-                });
-              }
-            });
-          });
+          ee.emit('s3done');
         }
       });
+
+      fs.writeFile(__dirname + '/../files/' + dirFile, req.body.content, function(err) {
+        if (err) {
+          sendRes(res, 'error');
+        } else {
+          ee.emit('hdDone');
+        }
+      });
+
+      ee.on('s3done', function() {
+        var file  = new File();
+        s3done    = true;
+        file.name = req.body.name;
+        User.findOne({name: req.params.user}, function(err, found) {
+          if (err) {
+            sendRes(res, 'error');
+          }
+          found.files.push(file._id);
+          file.user = found._id;
+          found.save(function(err, file) {
+            if (err) {
+              sendRes(res, 'error');
+            }
+          });
+          file.url = s3.getSignedUrl('getObject', {Bucket: parentBucket, Key: dirFile});
+          file.save(function(err, file) {
+            if (err) {
+              sendRes(res, 'error');
+            } else {
+              ee.emit('mongodbDone');
+            }
+          });
+        });
+      });
+      ee.on('mongodbDone', function() {
+        mongodbDone = true;
+        sendRes(res, message);
+      });
+      ee.on('hdDone', function() {
+        hdDone = true;
+        sendRes(res, message);
+      });
     })
+
     //deletes all files for a given user from the disc, mongodb and s3
     .delete(function(req, res) {
-      params = {Bucket: parentBucket};
-      params.Delete = {};
+      var ee                = new EventEmitter();
+      var message           = 'deleted all files for: ' + req.params.user;
+      s3done                = false;
+      mongodbDone           = false;
+      hdDone                = false;
+      var params            = {Bucket: parentBucket};
+      params.Delete         = {};
       params.Delete.Objects = [];
+
       fs.readdir(__dirname + '/../files/' + req.params.user, function(err, files) {
         if (err) {
-          res.send(err);
+          sendRes(res, 'error');
         } else {
           for (var i = 0; i < files.length; i++) {
             fs.unlinkSync(__dirname + '/../files/' + req.params.user + '/' + files[i]);
-            params.Delete.Objects.push({Key: req.params.user + '/' + files[i]});
+          }
+          ee.emit('hdDone');
+        }
+      });
+
+      s3.listObjects({Bucket: parentBucket, Prefix: req.params.user + '/'}, function(err, data) {
+        if (err) {
+          sendRes(res, 'error');
+        } else {
+          for (var i = 0; i < data.Contents.length; i++) {
+            params.Delete.Objects.push({Key: data.Contents[i].Key});
           }
           s3.deleteObjects(params, function(err, data) {
             if (err) {
-              res.send(err)
+              sendRes(res, 'error');
             } else {
-              User.findOne({name: req.params.user}, function (err, user){
-                if (err) {
-                  res.send(err);
-                } else {
-                  var ownerId = user._id;
-                  File.remove({user: ownerId}, function(err) {
-                    if (err) {
-                      res.send(err);
-                    } else {
-                      user.files = [];
-                      user.save();
-                      res.send({msg: 'deleted all files for: ' + req.params.user});
-                    }
-                  });
-                }
-              });
+              ee.emit('s3done');
             }
           });
         }
+      });
+
+      User.findOne({name: req.params.user}, function (err, user){
+        if (err) {
+          sendRes(res, 'error');
+        } else {
+          var ownerId = user._id;
+          File.remove({user: ownerId}, function(err) {
+            if (err) {
+              sendRes(res, 'error');
+            } else {
+              user.files = [];
+              user.save();
+              ee.emit('mongodbDone');
+            }
+          });
+        }
+      });
+
+      ee.on('s3done', function() {
+        s3done = true;
+        sendRes(res, message);
+      });
+      ee.on('mongodbDone', function() {
+        mongodbDone = true;
+        sendRes(res, message);
+      });
+      ee.on('hdDone', function() {
+        hdDone = true;
+        sendRes(res, message);
       });
     });
 
@@ -109,9 +175,9 @@ module.exports = function(router) {
       File.findOne({name: req.params.file})
             .exec(function(err, file) {
               if (err) {
-                res.send(err);
+                sendRes(res, 'error');
               }
-              res.send(file);
+              res.json(file);
             });
     })
     //updates a file across mongo, hard disc, s3
@@ -123,89 +189,141 @@ module.exports = function(router) {
     //old file in s3, creates a new file on disc and s3, and finally updates the
     //mongodb file listing.
     .put(function(req, res) {
+      var ee         = new EventEmitter();
+      var message    = 'Renamed: ' + req.params.file + ' to: ' + req.body.name;
+      var oldDirFile = req.params.user + '/' + req.params.file;
+      var newDirFile = req.params.user + '/' + req.body.name;
+      s3done         = false;
+      mongodbDone    = false;
+      hdDone         = false;
+
       if (!req.body.content && req.params.file !== req.body.name) {
-        fs.rename(__dirname + '/../files/' + req.params.user + '/' + req.params.file, __dirname + '/../files/' + req.params.user + '/' + req.body.name, function(err) {
+        fs.rename(__dirname + '/../files/' + oldDirFile, __dirname + '/../files/' + newDirFile, function(err) {
           if (err) {
-            res.send(err);
+            sendRes(res, 'error');
           } else {
-            s3.copyObject({Bucket: parentBucket, Key: req.params.user + '/' + req.body.name, CopySource: parentBucket + '/' + req.params.user + '/' + req.params.file }, function(err, data) {
+            ee.emit('hdDone');
+          }
+        });
+
+        s3.copyObject({Bucket: parentBucket, Key: newDirFile, CopySource: parentBucket + '/' + oldDirFile }, function(err, data) {
+          if (err) {
+            sendRes(res, 'error');
+          } else {
+            s3.deleteObject({Bucket: parentBucket, Key: oldDirFile}, function(err, data) {
               if (err) {
-                res.send(err);
+                sendRes(res, 'error');
               } else {
-                var newUrl = s3.getSignedUrl('getObject', {Bucket: parentBucket, Key: req.params.user +'/' + req.body.name});
-                File.update({name: req.params.file}, {name: req.body.name, url: newUrl}, function(err, file) {
-                  if (err) {
-                    res.send(err);
-                  } else {
-                    s3.deleteObject({Bucket: parentBucket, Key: req.params.user + '/' + req.params.file}, function(err, data) {
-                      if (err) {
-                        res.send(err);
-                      } else {
-                        res.send({msg: 'Renamed: ' + req.params.file + ' to: ' + req.body.name});
-                      }
-                    });
-                  }
-                });
+                ee.emit('s3doneName');
               }
             });
           }
         });
       } else {
-        fs.unlink(__dirname + '/../files/' + req.params.user +'/' + req.params.file, function(err){
+        fs.unlink(__dirname + '/../files/' + oldDirFile, function(err){
           if (err) {
-            res.send(err);
+            sendRes(res, 'error');
           } else {
-            fs.writeFile(__dirname + '/../files/' + req.params.user +'/' + req.body.name, req.body.content, function(err) {
+            fs.writeFile(__dirname + '/../files/' + newDirFile, req.body.content, function(err) {
               if (err) {
-                res.send(err);
+                sendRes(res, 'error');
               } else {
-                s3.putObject({ Bucket: parentBucket, Key: req.params.user +'/' + req.body.name, Body: req.body.content }, function(err, data) {
-                  if (err) {
-                    res.send(err);
-                  } else {
-                    var newUrl = s3.getSignedUrl('getObject', {Bucket: parentBucket, Key: req.params.user +'/' + req.body.name});
-                    File.update({name: req.params.file}, {name: req.body.name, url: newUrl}, function(err, file) {
-                      if (err) {
-                        res.send(err);
-                      } else {
-                        s3.deleteObject({Bucket: parentBucket, Key: req.params.user + '/' + req.params.file}, function(err, data) {
-                          if (err) {
-                            res.send(err);
-                          } else {
-                            res.send({msg: 'Renamed: ' + req.params.file + ' to: ' + req.body.name});
-                          }
-                        });
-                      }
-                    });
-                  }
-                });
+                ee.emit('hdDone');
+              }
+            });
+          }
+        });
+
+        s3.putObject({ Bucket: parentBucket, Key: newDirFile, Body: req.body.content }, function(err, data) {
+          if (err) {
+            sendRes(res, 'error');
+          } else {
+            s3.deleteObject({Bucket: parentBucket, Key: oldDirFile}, function(err, data) {
+              if (err) {
+                sendRes(res, 'error');
+              } else {
+                ee.emit('s3done');
               }
             });
           }
         });
       }
+
+      ee.on('s3doneName', function() {
+        s3done = true;
+        var newUrl = s3.getSignedUrl('getObject', {Bucket: parentBucket, Key: newDirFile});
+        File.update({name: req.params.file}, {name: req.body.name, url: newUrl}, function(err, file) {
+          if (err) {
+            sendRes(res, 'error');
+          } else {
+            ee.emit('mongodbDone');
+          }
+        });
+      });
+      ee.on('s3done', function() {
+        s3done = true;
+        var newUrl = s3.getSignedUrl('getObject', {Bucket: parentBucket, Key: newDirFile});
+        File.update({name: req.params.file}, {name: req.body.name, url: newUrl}, function(err, file) {
+          if (err) {
+            sendRes(res, 'error');
+          } else {
+            ee.emit('mongodbDone');
+          }
+        });
+      });
+      ee.on('mongodbDone', function() {
+        mongodbDone = true;
+        sendRes(res, message);
+      });
+      ee.on('hdDone', function() {
+        hdDone = true;
+        sendRes(res, message);
+      });
     })
     //Deletes specific file from mongodb, disc, and S3
     .delete(function(req, res) {
+      var ee      = new EventEmitter();
+      var message = 'successfully deleted: ' + req.params.file;
+      var dirFile = req.params.user + '/' + req.params.file;
+      s3done      = false;
+      mongodbDone = false;
+      hdDone      = false;
+
       File.remove({name: req.params.file}, function(err){
         if (err) {
-          res.send(err);
+          sendRes(res, 'error');
         } else {
-          fs.unlink(__dirname + '/../files/' + req.params.user +'/' + req.params.file, function(err){
-            if (err) {
-              res.send(err);
-            } else {
-              s3.deleteObject({Bucket: parentBucket, Key: req.params.user + '/' + req.params.file}, function(err, data) {
-                if (err) {
-                  res.send(err);
-                } else {
-                  res.send({msg: 'successfully deleted: ' + req.params.file});
-                }
-              });
-            }
-          });
+          ee.emit('mongodbDone');
         }
       });
-    });
 
+      fs.unlink(__dirname + '/../files/' + dirFile, function(err){
+        if (err) {
+          sendRes(res, 'error');
+        } else {
+          ee.emit('hdDone');
+        }
+      });
+
+      s3.deleteObject({Bucket: parentBucket, Key: dirFile}, function(err, data) {
+        if (err) {
+          sendRes(res, 'error');
+        } else {
+          ee.emit('s3done');
+        }
+      });
+
+      ee.on('s3done', function() {
+        s3done = true;
+        sendRes(res, message);
+      });
+      ee.on('mongodbDone', function() {
+        mongodbDone = true;
+        sendRes(res, message);
+      });
+      ee.on('hdDone', function() {
+        hdDone = true;
+        sendRes(res, message);
+      });
+    });
 };
